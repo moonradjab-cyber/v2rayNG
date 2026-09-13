c40971bf7de7ce4ab409ef3f15b0dc29d106e1bf
moonradjab-cyber <moonradjab@gmail.com>
2026-09-13 12:40:36 +0300

    Rebrand to Maxachkala VPN

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.maxachkala.vpn"
         minSdk = 24
         targetSdk = 37
         versionCode = 748
